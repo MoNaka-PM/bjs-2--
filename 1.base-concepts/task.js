03c4e458c539cf34389b94962ffc71bc70307faf
MoNaka <monaka.av@gmail.com>
2023-08-20 16:21:20 +0700

    task.js changed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  const Discriminant = b**2-4*a*c;
+ //no roots
+  if (Discriminant < 0) {
+   return arr;
+  } else if (Discriminant === 0) {
+const root = -b / (2 * a);
+arr.push(root);
+} else {
+   const root1 = (-b + Math.sqrt(Discriminant)) / (2 * a);
+   const root2 = (-b - Math.sqrt(Discriminant)) / (2 * a);
+   arr.push(root1, root2); 
+};
   return arr;
 }
 
