3830e847354b700fe5313606a6fa11afbe86a19a
MoNaka <monaka.av@gmail.com>
2023-08-23 23:51:59 +0700

    task.js changed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,13 +2,11 @@
 function solveEquation(a, b, c) {
   let arr = [];
   const Discriminant = b**2-4*a*c;
- //no roots
-  if (Discriminant < 0) {
-   return arr;
-  } else if (Discriminant === 0) {
+ 
+  if (Discriminant === 0) {
 const root = -b / (2 * a);
 arr.push(root);
-} else {
+} else if (Discriminant > 0){
    const root1 = (-b + Math.sqrt(Discriminant)) / (2 * a);
    const root2 = (-b - Math.sqrt(Discriminant)) / (2 * a);
    arr.push(root1, root2); 
@@ -17,17 +15,15 @@ arr.push(root);
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-   percent = +percent;
-   contribution = +contribution;
-   amount = +amount;
-   countMonths = +countMonths;
- if (typeof percent !== 'number'|| typeof contribution !== 'number'|| typeof amount !== 'number'|| typeof countMonths !== 'number') {
-   console.log ('false')
-} else {  
+   let result = [];
+ if (typeof parseFloat(percent) == 'number' && typeof parseFloat(contribution) == 'number' && typeof parseFloat(amount) == 'number' && typeof parseFloat(countMonths) == 'number') {
  let percentMounth = percent /100 / 12;
  let credit = amount - contribution;
  let payment = credit * (percentMounth + (percentMounth / (((1 + percentMounth)**countMonths) - 1)));
-   let TotalPayment = payment * countMonths;
-   console.log (Number(TotalPayment.toFixed(2)));
+let TotalPayment = payment * countMonths;
+   result = Number(result) + Number(TotalPayment.toFixed(2));
+ } else {
+   result = 'false';
  };
-}
+ return result;
+};
