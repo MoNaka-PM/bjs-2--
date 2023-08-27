eac52d74c949960b0dae8601d1bc43ebecc3b71d
MoNaka <monaka.av@gmail.com>
2023-08-27 15:18:28 +0700

    task1 in task.js changed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,20 @@
 function getArrayParams(...arr) {
-
+   let min, max, sum, avg;
+   sum = 0;
+   min = Infinity;
+   max = -Infinity;
+ 
+   for (let i = 0; i < arr.length; i++) {
+       if (arr[i] > max) {
+           max = arr[i]
+       } else if (arr[i] < min) {
+           min = arr[i]
+       }
+           sum  += arr[i];   
+   };
+       avg = (sum / arr.length).toFixed(2);
+       avg = Number(avg);
+       
   return { min: min, max: max, avg: avg };
 }
 
