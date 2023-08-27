922a563ced9cee87598fb48d6051773b1e46e575
MoNaka <monaka.av@gmail.com>
2023-08-27 20:06:38 +0700

    task5 in task.js changed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -73,5 +73,10 @@ return averageEEWorker;
 }
 
 function makeWork (arrOfArr, func) {
+ let maxWorkerResult = -Infinity;
+ for (let i = 0; i < arrOfArr.length; i++) {
 
+ };
+
+ return maxWorkerResult;
 }
